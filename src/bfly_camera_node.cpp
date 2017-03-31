46a4e42727e1c9366435f26c5c4a8c03e65b4bc7
andreucm <andreu@beta-robots.com>
2017-03-31 15:41:55 +0200

    Implementing the Camera Infor Set Service. Not yet tested. WIP

--- src/bfly_camera_node.cpp
+++ src/bfly_camera_node.cpp
@@ -2,7 +2,10 @@
 
 BflyCameraNode::BflyCameraNode() :
     nh_(ros::this_node::getName()), 
-    image_tp_(nh_)
+    image_tp_(nh_), 
+    matD(5,1,CV_64FC1),
+    matK(3,3,CV_64FC1),
+    matP(3,4,CV_64FC1)
 {
     //local vars
     BflyCamera::videoMode video_mode; 
@@ -13,14 +16,19 @@ BflyCameraNode::BflyCameraNode() :
     nh_.getParam("run_mode", param_int); this->run_mode_ = (RunMode)param_int; 
     nh_.getParam("rate", this->rate_);
     nh_.getParam("frame_name", this->camera_frame_name_);
+    nh_.getParam("camera_info_file", this->camera_info_file_);
     nh_.getParam("video_mode", param_int); video_mode = (BflyCamera::videoMode)param_int;
     nh_.getParam("pixel_format", param_int); pixel_format = (BflyCamera::pixelFormat)param_int;
     
     //init the image publisher
-    image_publisher_ = image_tp_.advertise("bfly_raw_image", 1);
+    image_publisher_ = image_tp_.advertise("image_raw", 1);
     
-    //init server
-    image_server_ = nh_.advertiseService("bfly_server", &BflyCameraNode::imageServiceCallback, this);    
+    //init the camera_info publisher
+    camera_info_publisher_ = nh_.advertise<sensor_msgs::CameraInfo>("camera_info", 1);
+    
+    //init servers
+    image_server_ = nh_.advertiseService("image_server", &BflyCameraNode::imageServiceCallback, this);
+    set_camera_info_server_ = nh_.advertiseService("set_camera_info_server", &BflyCameraNode::SetCameraInfoServiceCallback, this);        
     
     //constructs the camera object interfacing with HW
     camera_ = new BflyCamera::Device(); 
@@ -92,9 +100,20 @@ void BflyCameraNode::publish()
             break;            
     }
     
+    //fill the camera info
+    //TODO
+    camera_info_msg_.header.seq ++;
+    camera_info_msg_.header.stamp = ts;
+    camera_info_msg_.header.frame_id = camera_frame_name_; 
+//     camera_info_msg_.height = ...
+//     camera_info_msg_.width = ...
+//     ...
+    
     //publish the image    
     image_publisher_.publish(image_.toImageMsg());        
     
+    //publish the camera info
+    camera_info_publisher_.publish(camera_info_msg_);
 }
 
 bool BflyCameraNode::imageServiceCallback(
@@ -114,3 +133,48 @@ bool BflyCameraNode::imageServiceCallback(
     //return
     return true;     
 }
+
+bool BflyCameraNode::SetCameraInfoServiceCallback(
+            sensor_msgs::SetCameraInfo::Request  & _request, 
+            sensor_msgs::SetCameraInfo::Response & _reply)
+{
+    //create and open an openCV file
+    cv::FileStorage file(camera_info_file_, cv::FileStorage::WRITE); 
+    if ( ! file.isOpened() )
+    {
+        std::cout << "BflyCameraNode::BflyCameraNode(): Error opening file to store calibration data: " << camera_info_file_ << " not found." << std::endl;
+        _reply.success = false; 
+        _reply.status_message = -1; 
+        return false;
+    }
+    
+    //Fill the file with message content
+    time_t raw_time; 
+    time(&raw_time);
+    file << "date" << asctime(localtime(&raw_time));    
+    matD = (cv::Mat_<double>(5,1) << _request.camera_info.D[0],
+                                     _request.camera_info.D[1],
+                                     _request.camera_info.D[2],
+                                     _request.camera_info.D[3],
+                                     _request.camera_info.D[4]);
+    file << "paramsD" << matD;
+    matK = (cv::Mat_<double>(3,3) << _request.camera_info.K[0],_request.camera_info.K[1],_request.camera_info.K[2],
+                                     _request.camera_info.K[3],_request.camera_info.K[4],_request.camera_info.K[5],    
+                                     _request.camera_info.K[6],_request.camera_info.K[7],_request.camera_info.K[8]);
+    file << "matrixK" << matK;
+    matP = (cv::Mat_<double>(3,4) << _request.camera_info.R[0],_request.camera_info.R[1],_request.camera_info.R[2],_request.camera_info.R[3],
+                                     _request.camera_info.R[4],_request.camera_info.R[5],_request.camera_info.R[6],_request.camera_info.R[7],
+                                     _request.camera_info.R[8],_request.camera_info.R[9],_request.camera_info.R[10],_request.camera_info.R[11]);
+    file << "matrixP" << matP;
+    
+    //save the file
+    file.release();
+        
+    //reply the service
+    _reply.success = true; 
+    _reply.status_message = 1; 
+    
+    //return
+    return true; 
+}
+    
--- src/bfly_camera_node.h
+++ src/bfly_camera_node.h
@@ -12,6 +12,8 @@
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/SetCameraInfo.h>
 
 //custom ROS dependencies
 #include "ptgrey_bfly_camera/ImageAsService.h" //custom "capture" service
@@ -42,18 +44,29 @@ class BflyCameraNode
         
         //capture server
         ros::ServiceServer image_server_; 
+
+        //set camera info server
+        ros::ServiceServer set_camera_info_server_; 
         
-        //image transport and publisher
+        //image transport, publisher and message
         image_transport::ImageTransport image_tp_;
         image_transport::Publisher image_publisher_;      
-
-        //published image
         cv_bridge::CvImage image_;        
+        
+        //camera info publisher and message
+        ros::Publisher camera_info_publisher_;
+        sensor_msgs::CameraInfo camera_info_msg_;        
         
         //node configuration parameters
         RunMode run_mode_;//run mode: The node acts as a server, or a continuous pcl publisher        
         double rate_; //loop rate
         std::string camera_frame_name_; //name of the camera frame of reference
+        std::string camera_info_file_; //name of the camera info file. Where calibration data is stored
+        
+        //Calibration data. Get from a yaml file
+        cv::Mat matD;
+        cv::Mat matK;
+        cv::Mat matP;
         
     public:
         //constructor
@@ -75,6 +88,11 @@ class BflyCameraNode
         //Service callback implementing the image capture
         bool imageServiceCallback(ptgrey_bfly_camera::ImageAsService::Request  & _request, 
                                   ptgrey_bfly_camera::ImageAsService::Response & _reply);
+        
+        //Service to set the camera info data. Stores data in a yaml file
+        bool SetCameraInfoServiceCallback(sensor_msgs::SetCameraInfo::Request  & _request, 
+                                          sensor_msgs::SetCameraInfo::Response & _reply);
+        
                 
 };
 #endif
